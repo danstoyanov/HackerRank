9f9c88261da4ad33f183c2157584cfb455c8bc56
danstoyanov <dan.stoyanov@abv.bg>
2021-02-12 14:13:09 +0200

    update

---
+++ Problem Solving (Basic)/Problem Solving (Basic problems)/Birthday Cake Candles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Birthday_Cake_Candles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Problem Solving (Basic)/Problem Solving (Basic problems)/Problem Solving (Basic problems)/Program.cs
@@ -0,0 +1,12 @@
+﻿sing System;
+
+namespace Problem_Solving__Basic_problems_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
